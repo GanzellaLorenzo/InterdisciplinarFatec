7dad7a603ad85063c806cad1a3d923aefeb5962a
Lorenzo Ganzella <ganzellalorenzo@gmail.com>
2025-06-11 16:40:35 -0300

    feat: de ordenação por id no produto

--- backend/src/main/java/com/estoque/projeto/repository/ProdutoRepository.java
+++ backend/src/main/java/com/estoque/projeto/repository/ProdutoRepository.java
@@ -1,5 +1,7 @@
 package com.estoque.projeto.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.estoque.projeto.entity.ProdutoEntity;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoEntity,Integer> {
+    Page<ProdutoEntity> findAllByOrderByProdIdDesc(Pageable pageable);
 
 }
