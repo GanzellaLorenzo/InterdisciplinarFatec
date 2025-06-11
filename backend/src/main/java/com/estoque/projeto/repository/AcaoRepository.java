08788763f9cffc8d8ec97081908a2cf2eb74250e
Lorenzo Ganzella <ganzellalorenzo@gmail.com>
2025-06-11 16:39:33 -0300

    fix: ordenação por ID em movimentacoes

--- backend/src/main/java/com/estoque/projeto/repository/AcaoRepository.java
+++ backend/src/main/java/com/estoque/projeto/repository/AcaoRepository.java
@@ -1,5 +1,7 @@
 package com.estoque.projeto.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.estoque.projeto.entity.AcaoEntity;
 
 @Repository
 public interface AcaoRepository extends JpaRepository<AcaoEntity, Long> {
+    Page<AcaoEntity> findAllByOrderByIdDesc(Pageable pageable);
 }
